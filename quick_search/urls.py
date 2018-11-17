321145269d91c3d8b6e0401c0a513b6479ae1905
Kanika Modi <modkanik@amazon.com>
2018-11-18 03:17:23 +0530

    Embedding JS Code

--- quick_search/urls.py
+++ quick_search/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.search, name='search'),
+    path('js', views.get_js, name='get_js'),
+    path('css', views.get_css, name='get_css'),
+    path('html', views.get_html, name='get_html'),
 ]
--- quick_search/views.py
+++ quick_search/views.py
@@ -12,16 +12,23 @@ base_url = 'www.google.com/search?q='
 def search(request):
     body_json = json.loads(request.body.decode("utf-8"))
     query = body_json['query']
-<<<<<<< Updated upstream
     results = get_search_results(query)
     context = {"search_results":results}
     return render(request, "quick_search/search_results.html", context)
- 
-=======
-    print(query)
-    # return HttpResponse(query)
-    results = get_search_results(query)
-    context = {"search_results":results}
-    return render(request, "quick_search/search_results.html", context)
-    # return HttpResponse(get_search_html(query))
->>>>>>> Stashed changes
+
+def get_css(request):
+    test_file = open('static/quick_search.css', 'rb')
+    response = HttpResponse(content=test_file)
+    return response
+
+def get_js(request):
+    content = ""
+    with open("static/quick_search.js") as f:
+        content = f.read()
+    response = HttpResponse(content=content)
+    return response
+
+def get_html(request):
+    test_file = open('static/quick_search.html', 'rb')
+    response = HttpResponse(content=test_file)
+    return response

f9a73353762a508902ae9658d07e0a941d371c57
Naman Maheshwari <naman1901@gmail.com>
2018-11-18 02:04:11 +0530

    removed some comments

--- quick_search/views.py
+++ quick_search/views.py
@@ -12,10 +12,7 @@ base_url = 'www.google.com/search?q='
 def search(request):
     body_json = json.loads(request.body.decode("utf-8"))
     query = body_json['query']
-    # print(query)
-    # return HttpResponse(query)
     results = get_search_results(query)
     context = {"search_results":results}
     return render(request, "quick_search/search_results.html", context)
-    # return HttpResponse(get_search_html(query))
  
